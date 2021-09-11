1560f827518ce47569e605cb6b7441a799ff1bd1
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-09-11 22:28:10 +0530

    drawer navigation added

--- App.js
+++ App.js
@@ -3,21 +3,45 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
+  Button,
+  View,
   Text,
 } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 const  App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={'light-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        >
-        <Text>sdds</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  </NavigationContainer>
   );
 };
+
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
 
 export default App;
--- android/app/src/main/java/com/keells_rewamp/MainActivity.java
+++ android/app/src/main/java/com/keells_rewamp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.keells_rewamp;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +11,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "keells_rewamp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
