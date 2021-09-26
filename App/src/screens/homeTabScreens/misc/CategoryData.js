fae90faa3a84de65dcb59d234f585c5670c02a0b
Shihara <shiharadotadilshanid@gmail.com>
2021-09-26 20:47:59 +0530

    minor changes

---
+++ App/src/screens/homeTabScreens/misc/CategoryData.js
@@ -0,0 +1,63 @@
+export default categoryData = [
+  {
+    isExpand: false,
+    category_name: 'Grocery',
+    subcategory: [
+      {id: 1, val: 'All'},
+      {id: 2, val: 'Pulses'},
+      {id: 3, val: 'Sugar'},
+      {id: 4, val: 'Flour'},
+      {id: 5, val: 'Pasta/Noodles'},
+    ],
+  },
+  {
+    isExpand: false,
+    category_name: 'Vegitables',
+    subcategory: [
+      {id: 6, val: 'All'},
+      {id: 7, val: 'Pola'},
+      {id: 8, val: 'Up Country'},
+      {id: 9, val: 'Low Country'},
+      {id: 10, val: 'Packed & Units'},
+    ],
+  },
+  {
+    isExpand: false,
+    category_name: 'Fruits',
+    subcategory: [
+      {id: 11, val: 'All'},
+      {id: 12, val: 'Inorganic'},
+      {id: 13, val: 'Packed & Units'},
+    ],
+  },
+  {
+    isExpand: false,
+    category_name: 'Meat',
+    subcategory: [
+      {id: 14, val: 'All'},
+      {id: 15, val: 'Fresh Meat'},
+      {id: 16, val: 'Frozen Meat'},
+    ],
+  },
+  {
+    isExpand: false,
+    category_name: 'Fish',
+
+    subcategory: [
+      {id: 17, val: 'All'},
+      {id: 18, val: 'Fresh Sea Food'},
+    ],
+  },
+  {
+    isExpand: false,
+    category_name: 'Beverages',
+    subcategory: [
+      {id: 19, val: 'All'},
+      {id: 20, val: 'Juices & Carbonates'},
+      {id: 21, val: 'Malt Drinks'},
+      {id: 22, val: 'Sports & Energy Drinks'},
+      {id: 23, val: 'Water'},
+      {id: 24, val: 'Cofee'},
+    ],
+  },
+];
